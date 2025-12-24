3984e915ac964bc05d99649061579d5f7e705ac6
iamanandri <irfananandri@gmail.com>
2025-12-24 12:35:20 +0700

    Made the commands cleaner with routing via match statement and functions

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 import sys
 
-todolist = []
-    
 def main():
-    
-    menulist = (
-        "Add task",
-        "Edit task",
-        "Remove task",
-    )
-    task = ""
+    todolist = []
     active = True
     command = ""
 
     print("Welcome to the Obligatory To Do List App!")
+    print("Type: \"add what you need to do\" to add your first task!")
 
     while(active):    
         ### LISTING OUT ALL THE TASKS
@@ -21,47 +14,39 @@ def main():
             print("### YOUR TASKS ###")
             listTask(todolist)
 
-
         command = input()
 
-        if "list" in command.lower():
-            listTask(todolist)
+        match command.partition(" ")[0].lower():
+            case "add":
+                addTask(todolist, command)
+            case "remove":
+                removeTask(todolist, int(command.partition(" ")[2])-1)
+            case "edit":
+                editTask(todolist, command.partition(" ")[2])
+            case "list":
+                listTask(todolist)
+            case "quit":
+                print("All in a day's work, good byeeeee ^^")
+                active = False
+                sys.exit()
+            case default:
+                print("I don't recognize that function?")
+                
+def addTask(todolist, command):
+    task = command.partition(" ")[2].strip("\"")
+    todolist.append(task)
 
-        elif "add" in command.lower():
-            task = command.partition(" ")[2].strip("\"")
-            todolist.append(task)
-            
-        elif "remove" in command.lower():
-            if(len(todolist) > 0):
-                taskIndex = int(command.partition(" ")[2])-1
-                removedtask = todolist[taskIndex]
-                todolist.pop(taskIndex)
-                print(f"\nYou've removed {removedtask}\n")
-                removedtask = ""
-            else:
-                print("\n>> You have no task to remove! Please add a task first ^^!\n")
-        
-        elif "edit" in command.lower():
-            if(len(todolist) > 0):
-                # [edit] [1 weewoo weewoo]
-                # [1] [wee woo wee woo]
-                task = command.partition(" ")[2]
-                newTaskIndex = int(task.partition(" ")[0])-1
-                newTask = task.partition(" ")[2]
-                todolist[newTaskIndex] = newTask
-            else:
-                print("\n>> You have no task to remove! Please add a task first ^^!\n")
-        
-        elif "quit" in command.lower():
-            print("All in a day's work, good byeeeee ^^")
-            active = False
-            sys.exit()
-                    
-### Operation functions! Can this be a util class? 
+def removeTask(todolist, taskindex):
+    todolist.pop(taskindex)
+
+def editTask(todolist, commandArgs):
+    taskIndex = int(commandArgs.partition(" ")[0])-1
+    newTask = commandArgs.partition(" ")[2]
+    todolist[taskIndex] = newTask
+
 def listTask(listitem):
     for i in range(len(listitem)):
         print(f"{i+1}. {listitem[i]}")
 
 if __name__ == "__main__":
-    app = main()
-    app.run()
+    main()
